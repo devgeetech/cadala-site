e5c530a8ec488f2b02f7b0b0cef88c4a1a35f2fd
devgeetech <devgeetech@gmail.com>
2020-07-03 04:43:58 +0530

    with public folder

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -3,13 +3,17 @@ import React from "react"
 import NavigationItems from '../../navigation/navigationItems'
 
 import cadalaLogo from '../../../images/Logo.svg'
+import menuIcon from '../../../images/menuIcon.svg'
 
 const Header = () => {
     return(
         <div className="header">
             <div className="header__wrap">
+                <div className="header__wrap__menu">
+                    <img src={menuIcon} alt="Menu Icon" className="header__wrap__menu__img" />
+                </div>
                 <div className="header__wrap__logo">
-                    <img src={cadalaLogo} alt="Cadala Logo" className="header__logo__img" />
+                    <img src={cadalaLogo} alt="Cadala Logo" className="header__wrap__logo__img" />
                 </div>
                 <div className="header__wrap__nav-items">
                     <NavigationItems />
