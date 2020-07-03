f6ea0cb8d4f91f112d3ba18943853f0dae3b4ead
devgeetech <devgeetech@gmail.com>
2020-07-03 06:50:37 +0530

    vendor form

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,83 @@ const IndexPage = () => (
             </div>
           </div>
         </div>
+        
+        
+        <div className="index__vendor">
+          <div className="index__vendor__content">
+            <div className="index__vendor__content__text">
+              <div className="index__vendor__content__text__head">
+                Join as a B2B Merchant
+              </div>
+              <div className="index__vendor__content__text__desc">
+                Join a revolutionary marketplace that showcases the many variety of things 
+                Indian manufacturers offer.<br/> <br/>
+
+                Get more orders and reach with new business models like dropshipping. 
+                Export private labelled products and expand your business empire!
+              </div>
+            </div>
+
+            <div className="index__vendor__content__form">
+              <form className="index__vendor__content__form__actual">
+                <div className="index__vendor__content__form__actual__fields">
+                  <div className="index__vendor__content__form__actual__fields__column">
+                    <input 
+                      type="text" 
+                      placeholder="Name" 
+                      aria-label="Name field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>
+                    <input 
+                      type="text" 
+                      placeholder="Company Name" 
+                      aria-label="Company field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/> 
+                    <input 
+                      type="text" 
+                      placeholder="Address Line 1" 
+                      aria-label="Address 1 field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>
+                    <input 
+                      type="text" 
+                      placeholder="Address Line 1" 
+                      aria-label="Address 2 field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>  
+                  </div>
+                  <div className="index__vendor__content__form__actual__fields__column">
+                    <input 
+                      type="text" 
+                      placeholder="City" 
+                      aria-label="City field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>
+                    <input 
+                      type="text" 
+                      placeholder="State" 
+                      aria-label="State field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/> 
+                    <input 
+                      type="tel" 
+                      placeholder="Phone" 
+                      aria-label="Phone field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>
+                    <input 
+                      type="email" 
+                      placeholder="Email" 
+                      aria-label="Email field" 
+                      className="index__vendor__content__form__actual__fields__column__input"/>  
+                  </div>
+                </div>
+                <div className="index__vendor__content__form__actual__submit">
+                  <input type="submit" className="index__vendor__content__form__actual__submit__button"/>
+                </div>
+                <div className="index__vendor__content__form__actual__tnc">
+                  *By joining you agree to our terms and conditions &amp; privacy policy
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>   
+
+
     </div>
   </Layout>
 )
